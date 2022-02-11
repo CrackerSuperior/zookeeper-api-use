44a57d9c717db46401b7c19489203c70a62c6120
Cracker <2278801557@qq.com>
2022-02-11 14:54:24 +0800

    add zookeeper node get data api usage and test code.

--- src/main/java/com/cracker/zookeeper/api/node/ZooKeeperNode.java
+++ src/main/java/com/cracker/zookeeper/api/node/ZooKeeperNode.java
@@ -5,8 +5,10 @@ import lombok.Getter;
 import lombok.Setter;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooDefs.Ids;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
 
 @Setter
 @Getter
@@ -26,5 +28,27 @@ public class ZooKeeperNode {
         zooKeeper.create(path, data, Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
     }
     
+    /**
+     * Check existence of ZNode and its status, if ZNode is available.
+     * @param path ZNode path
+     * @return ZNode status
+     * @throws InterruptedException if the thread is interrupted
+     * @throws KeeperException zookeeper exception
+     */
+    public Stat exists(final String path) throws InterruptedException, KeeperException {
+        return zooKeeper.exists(path, true);
+    }
     
+    /**
+     * Get the data attached in a specified ZNode and its status.
+     * @param path ZNode path
+     * @param watcher callback function of type Watcher. The ZooKeeper ensemble will notify through the Watcher callback when the data of the specified ZNode changes. This is one-time notification
+     * @param stat returns the metadata of a ZNode.
+     * @return ZNode data
+     * @throws InterruptedException if the thread is interrupted
+     * @throws KeeperException zookeeper exception
+     */
+    public byte[] getData(final String path, Watcher watcher, Stat stat) throws InterruptedException, KeeperException {
+        return zooKeeper.getData(path, watcher, stat);
+    }
 }
--- src/test/java/com/cracker/zookeeper/api/node/ZooKeeperNodeTest.java
+++ src/test/java/com/cracker/zookeeper/api/node/ZooKeeperNodeTest.java
@@ -1,8 +1,14 @@
 package com.cracker.zookeeper.api.node;
 
 import com.cracker.zookeeper.api.connect.ZooKeeperConnection;
+import org.apache.zookeeper.Watcher.Event.EventType;
+import org.apache.zookeeper.Watcher.Event.KeeperState;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
 import org.junit.Test;
+
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CountDownLatch;
 
 public class ZooKeeperNodeTest {
     
@@ -15,6 +21,63 @@ public class ZooKeeperNodeTest {
             ZooKeeper zooKeeper = zooKeeperConnection.connect("localhost");
             ZooKeeperNode zooKeeperNode = new ZooKeeperNode(zooKeeper);
             zooKeeperNode.create(path, data);
+            zooKeeperConnection.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    @Test
+    public void existsNode() {
+        String path = "/MyFirstZNode";
+        try {
+            ZooKeeperConnection zooKeeperConnection = new ZooKeeperConnection();
+            ZooKeeper zooKeeper = zooKeeperConnection.connect("localhost");
+            ZooKeeperNode zooKeeperNode = new ZooKeeperNode(zooKeeper);
+            Stat stat = zooKeeperNode.exists(path);
+            if(stat != null) {
+                System.out.println("Node exists and the node version is " + stat.getVersion());
+            } else {
+                System.out.println("Node does not exists");
+            }
+            zooKeeperConnection.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    @Test
+    public void getNodeData() {
+        String path = "/MyFirstZNode";
+        CountDownLatch connectedSignal = new CountDownLatch(1);
+        try {
+            ZooKeeperConnection zooKeeperConnection = new ZooKeeperConnection();
+            ZooKeeper zooKeeper = zooKeeperConnection.connect("localhost");
+            ZooKeeperNode zooKeeperNode = new ZooKeeperNode(zooKeeper);
+            if (zooKeeperNode.exists(path) != null) {
+                byte[] bytes = zooKeeperNode.getData(path, watchedEvent -> {
+                    if (watchedEvent.getType() == EventType.None) {
+                        if (watchedEvent.getState() == KeeperState.Expired) {
+                            connectedSignal.countDown();
+                        }
+                    } else {
+                        try {
+                            byte[] tempBytes = zooKeeper.getData(path, false, null);
+                            String data = new String(tempBytes, StandardCharsets.UTF_8);
+                            System.out.println("data1" + data);
+                            connectedSignal.countDown();
+                        } catch (Exception e) {
+                            System.out.println(e.getMessage());
+                        }
+                    }
+                }, null);
+                connectedSignal.countDown();
+                String data = new String(bytes, StandardCharsets.UTF_8);
+                System.out.println("data2" + data);
+                connectedSignal.await();
+            } else {
+                System.out.println("Node does not exists");
+            }
             zooKeeperConnection.close();
         } catch (Exception e) {
             System.out.println(e.getMessage());
